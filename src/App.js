832837126cd8c3fa88acd700ff265d0147a74442
unknown <infovinodjangid@gmail.com>
2023-08-16 17:40:54 +0530

    Error Fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 // import './Styling/Navbar.css';
 // import './Styling/Footer.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Preloader from './Components/Preloader';
+
 import Home from './Routes/Home';
 import About from './Routes/About';
 import Footer from './Components/Footer';
@@ -12,15 +12,9 @@ import ScrollToTop from './Components/ScrollToTop';
 
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  window.onload = () => {
-    setLoading(false);
-  };
+  
   return (
     <>
-      {loading ? (
-        <Preloader />
-      ) : (
         <Router>
           <ScrollToTop />
           <Navbar />
@@ -30,8 +24,7 @@ function App() {
           </Routes>
           <Footer />
         </Router>
-      )}
-      
+     
     </>
   );
 }
